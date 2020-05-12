b1b285c6bd5e73c515909e09565e05513b7088c7
Harsh Kumar <harshkmr991234@gmail.com>
2020-05-12 23:44:01 +0530

    Exercise: Controlled Forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,57 +1,206 @@
-import React from "react";
+import React, { Component } from "react";
+import { Form, FormGroup, Label, Col, Input, Button } from "reactstrap";
 
-function Contact(props) {
-  console.log("Contact");
-  return (
-    <div className="container">
-      <div className="row row-content">
-        <div className="col-12">
-          <h3>Location Information</h3>
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: "",
+      lastname: "",
+      telnum: "",
+      email: "",
+      agree: false,
+      contactType: "Tel.",
+      message: ""
+    };
+  }
+
+  handleInputChange = event => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = event => {
+    console.log(JSON.stringify(this.state));
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row row-content">
+          <div className="col-12">
+            <h3>Location Information</h3>
+          </div>
+          <div className="col-12 col-sm-4 offset-sm-1">
+            <h5>Our Address</h5>
+            <address>
+              121, Clear Water Bay Road
+              <br />
+              Clear Water Bay, Kowloon
+              <br />
+              HONG KONG
+              <br />
+              <i className="fa fa-phone" />: +852 1234 5678
+              <br />
+              <i className="fa fa-fax" />: +852 8765 4321
+              <br />
+              <i className="fa fa-envelope" />:{" "}
+              <a href="mailto:confusion@food.net">confusion@food.net</a>
+            </address>
+          </div>
+          <div className="col-12 col-sm-6 offset-sm-1">
+            <h5>Map of our Location</h5>
+          </div>
+          <div className="col-12 col-sm-11 offset-sm-1">
+            <div className="btn-group" role="group">
+              <a
+                role="button"
+                className="btn btn-primary"
+                href="tel:+85212345678"
+              >
+                <i className="fa fa-phone" /> Call
+              </a>
+              <a role="button" className="btn btn-info">
+                <i className="fa fa-skype" /> Skype
+              </a>
+              <a
+                role="button"
+                className="btn btn-success"
+                href="mailto:confusion@food.net"
+              >
+                <i className="fa fa-envelope-o" /> Email
+              </a>
+            </div>
+          </div>
         </div>
-        <div className="col-12 col-sm-4 offset-sm-1">
-          <h5>Our Address</h5>
-          <address>
-            121, Clear Water Bay Road
-            <br />
-            Clear Water Bay, Kowloon
-            <br />
-            HONG KONG
-            <br />
-            <i className="fa fa-phone" />: +852 1234 5678
-            <br />
-            <i className="fa fa-fax" />: +852 8765 4321
-            <br />
-            <i className="fa fa-envelope" />:{" "}
-            <a href="mailto:confusion@food.net">confusion@food.net</a>
-          </address>
-        </div>
-        <div className="col-12 col-sm-6 offset-sm-1">
-          <h5>Map of our Location</h5>
-        </div>
-        <div className="col-12 col-sm-11 offset-sm-1">
-          <div className="btn-group" role="group">
-            <a
-              role="button"
-              className="btn btn-primary"
-              href="tel:+85212345678"
-            >
-              <i className="fa fa-phone" /> Call
-            </a>
-            <a role="button" className="btn btn-info">
-              <i className="fa fa-skype" /> Skype
-            </a>
-            <a
-              role="button"
-              className="btn btn-success"
-              href="mailto:confusion@food.net"
-            >
-              <i className="fa fa-envelope-o" /> Email
-            </a>
+
+        <div className="row row-content">
+          <div className="col-12">
+            <h3>Send us Your Feedback</h3>
+          </div>
+          <div className="col-12 col-md-9">
+            <Form onSubmit={this.handleSubmit}>
+              <FormGroup row>
+                <Label htmlFor="firstname" md={2}>
+                  First Name
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="text"
+                    id="firstname"
+                    name="firstname"
+                    placeholder="First Name"
+                    onChange={this.handleInputChange}
+                    value={this.state.firstname}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="lastname" md={2}>
+                  Last Name
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="text"
+                    id="lastname"
+                    name="lastname"
+                    placeholder="Last Name"
+                    onChange={this.handleInputChange}
+                    value={this.state.lastname}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="telnum" md={2}>
+                  Contact No.
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="tel"
+                    id="telnum"
+                    name="telnum"
+                    placeholder="Contact Number"
+                    onChange={this.handleInputChange}
+                    value={this.state.telnum}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="email" md={2}>
+                  Email
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email Address"
+                    onChange={this.handleInputChange}
+                    value={this.state.email}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col md={{ size: 6, offset: 2 }}>
+                  <FormGroup check>
+                    <Label check>
+                      <Input
+                        type="checkbox"
+                        name="agree"
+                        onChange={this.handleInputChange}
+                        checked={this.state.agree}
+                      />
+                      <strong>We may contact you</strong>
+                    </Label>
+                  </FormGroup>
+                </Col>
+                <Col md={{ size: 3, offset: 1 }}>
+                  <Input
+                    type="select"
+                    name="contactType"
+                    onChange={this.handleInputChange}
+                    value={this.state.contactType}
+                  >
+                    <option>Tel.</option>
+                    <option>Email</option>
+                  </Input>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="message" md={2}>
+                  Your Feedback
+                </Label>
+                <Col md={10}>
+                  <Input
+                    type="textarea"
+                    id="message"
+                    name="message"
+                    rows="6"
+                    placeholder="Your Feedback"
+                    onChange={this.handleInputChange}
+                    value={this.state.message}
+                  />
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Col md={{ size: 10, offset: 2 }}>
+                  <Button type="submit" color="dark">
+                    Send Feedback
+                  </Button>
+                </Col>
+              </FormGroup>
+            </Form>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Contact;
